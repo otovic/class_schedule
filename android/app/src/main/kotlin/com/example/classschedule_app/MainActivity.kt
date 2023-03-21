fee3553647e9b65c118eaf11d3b84c030550cd5d
PetarOtovic <otovicpetar1998@gmail.com>
2023-03-20 20:12:30 -0700

    Init

---
+++ android/app/src/main/kotlin/com/example/classschedule_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.classschedule_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main() {
+
+}
+
+
+
